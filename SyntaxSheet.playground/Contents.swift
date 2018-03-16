dc8464cbbcb3da5532aaf961170e65e412ab9649
Ben Hall <b.e.s.hall@outlook.com>
2018-03-16 15:25:26 +0000

    Added enums, conditional statements, loops, switch-case, functions,  optionals

--- SyntaxSheet.playground/Contents.swift
+++ SyntaxSheet.playground/Contents.swift
@@ -35,6 +35,9 @@ let double: Double = 1.0 // Swift defaults to a double with type inference
 let bool: Bool = false // or true
 
 // Optional
+// An optional is a special swift type that 'may' have a value of the specified type.
+// Otherwise it is nil. Think of the syntax as asking whether the variable contains a value of not.
+// More on this later, here is the syntax.
 var optional: String? = nil // or any <type>?
 
 // =====================
@@ -112,6 +115,217 @@ let dictKeys = Array(personDict.keys)
 
 // As with arrays, there are many Type methods for dictionaries for manipulating the elements.
 
+// ENUMS - A first class Type that allows you to create own types
+enum WeatherType {
+    case sun
+    case cloud
+    case rain
+    case wind
+    case snow
+}
 
+let weather: WeatherType = .sun
+
+switch weather {
+case .sun: print("Shorts time!") // Note the type inference of 'weather' - no need to specify 'WeatherType.sun' in the case!
+case .cloud: print("Meh...")
+case .rain: print("Pack an umbrella!")
+case .wind: print("Maybe time to fly a kite?")
+case .snow: print("SNOWDAY!")
+}
+
+
+// Enums can also have values attached to their them!
+enum AnotherWeatherType {
+    case sun
+    case cloud
+    case rain
+    case wind(speed: Int)
+    case snow
+}
+
+let windyWeather: AnotherWeatherType = .wind(speed: 5)
+
+switch windyWeather {
+case .wind(let whatSpeed) where whatSpeed < 10:
+    print("A little breezy")
+case .wind(let whatSpeed) where whatSpeed > 10:
+    print("WINDY!")
+default: print("") // REMEMBER: Switch-case must be exhaustive
+}
+
+
+// ==============================
+// Conditional Statements (logic)
+// ==============================
+var logicTest: Bool = true
+if logicTest {
+    print("Logical!")
+} else {
+    print("Not so logical!")
+}
+
+// Can have multiple logical statements including || (or), && (and)
+
+// ==============================
+// Loops
+// ==============================
+
+// For loops
+// - Through a range
+for _ in 1 ... 5 { // _ if not interested in the counter. x ... y is a countable range from x to, and including, y
+ print("Spam")
+}
+
+for i in 1 ..< 5 { // setting a variable for range counter allows us to use it. x ..< y is another countable range from x up to, but not including y.
+    print(i)
+}
+
+// - Through an array
+let nameArray = ["Jane", "Joe"]
+for name in nameArray {
+    print(name)
+}
+
+// or...
+for arrIndex in 0 ..< nameArray.count {
+    print(nameArray[arrIndex])
+}
+
+// or.. enumerate for both!
+for (index,entry) in nameArray.enumerated() {
+    print("Name at index \(index) is: \(entry)")
+}
+
+// While Loops
+// run until condition is invalidated, or loop broken from
+var whileCnt = 0
+while true {
+    if whileCnt == TATLTUAE {
+        print("It took \(whileCnt) iterations to reach TATLTUAE")
+        break
+    }
+    
+    whileCnt += 1
+}
+
+
+// ==============================
+// Switch - Case
+// ==============================
+// Another type of conditional logic
+// Evaluates a series of 'case' series until a condition is met, then 'switches' that code on.
+// No fallthrough unless specifically stated
+// Must be exhaustive (i.e. include all enum options, type possibilities, or a default condition)
+let cases: Int = 3
+switch cases {
+case 0:
+    print("Switched on case 0")
+case 1:
+    print("Switched on case 1")
+case 2:
+    print("Switched on case 2")
+case 3:
+    print("Switched on case 3 with fallthrough")
+    fallthrough
+default:
+    print("Default switch")
+}
+
+// The case can also be a countable range
+switch cases {
+case 0 ..< 3:
+    print("Case in within range 0 - 2 (inclusive)")
+default:
+    print("Case is >2")
+}
+
+
+// ==============================
+// Functions
+// ==============================
+// Re-usable code snippets
+// Really, I function should do one thing, and only that thing.
+// This should mean the function is clear, and short.
+// Non-returning
+func add(value a: Int, to b: Int) -> () {
+    print("\(a) + \(b) = \(a+b)")
+}
+
+add(value: 1, to: 10)
+
+// A returning function has the returning type declared
+func addAndReturn(value a: Int, to b: Int) -> Int {
+    return(a+b)
+}
+print("10 + 20 = \(addAndReturn(value: 10, to: 20))")
+
+"""
+    The add functions above have used 'external' (e.g. 'value' and 'to')
+    and 'internal' parameter names (e.g. 'a' and 'b')
+    EXTERNAL parameter names makes it easier to describe descriptive api functions
+    whilst INTERNAL names can be shorthand for actual function implementation
+    Putting an "_" as the external name tells swift that there should be no EXTERNAL name required (e.g. call for add would be add(1,2))
+    Functions can be combined with switch statements to good effect!
+"""
+
+// ==============================
+// More on Optionals
+// ==============================
+func isSwiftOptional(answer: Bool) -> String? {
+    if answer {
+        return nil
+    } else {
+        return "Correct. Swift is too awesome to be optional (OPTIONAL UNPACK ME!)"
+    }
+}
+
+let wrappedOutput: String?
+wrappedOutput = isSwiftOptional(answer: false)
+// this will need to be unpacked
+// if we know it has a value we use the syntax:
+print(wrappedOutput!)
+// If we want to be safer we use 'if let' statement
+if let unwrappedOutput = wrappedOutput {
+    print("If let unwrapped contained \"\(unwrappedOutput)\"")
+} else {
+    print("The value is nil")
+}
+
+// Or a variant that returns is the guard statement
+func guardUnwrapperFunction() {
+    guard let unwrappedOutput = wrappedOutput else {
+        print("This code is executed if an optional CANNOT be unwrapped (i.e. is nil)")
+        return
+    }
+    print("But now have acess to the unwrappedOutput,\n\t\"\(unwrappedOutput)\"\nbeyond any condition statement blocks.")
+}
+
+guardUnwrapperFunction()
+
+
+// Optional chaining
+// Allows us to evaluate whether a chain of commands may fail at some point due to existence of optionals (or rahter absence of an object/var)
+// For example, say we have this function that can return an optional
+func canReturnOptional(switchOn: Int) -> String? {
+    switch switchOn {
+    case 0 ... 2: return "A string"
+    default: return nil
+    }
+}
+
+// Based on the input to this function a nil could be returned instead of a String so explicit unpacking can be dangerous.
+// Also if we chained a type method after this (e.g. uppercased() to make the string uppercase) it will fail without some form of unpacking unless we chain it!
+// Like so:
+// let willFailFromUnpackedOptional = canReturnOptional(switchOn: 0).uppercased()
+let possibleNilOrString = canReturnOptional(switchOn: 3)?.uppercased()
+let anotherPossibleNilOrString = canReturnOptional(switchOn: 0)?.uppercased()
+
+// The Nil Coalescing Operator allows us to evaluate the existence of an unpackable optional (non nil), but if nil assign a defined output
+// Sytax is: nonOptionalOutput = varOrFuncThatMayReturnOptional ?? returnedValueIfPriorEvaluatedToNil
+// ?? is the NCO
+let unpackedOptionalOrNCO = (canReturnOptional(switchOn: 3) ?? "unknown").uppercased()
+
+// so the constant possibleOptional will be an optional in this case
 print("Done")
 
