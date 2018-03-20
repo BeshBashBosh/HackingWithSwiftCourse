03a97bc4cd124ccda29c47db133cad421ae5ac3f
Ben Hall <b.e.s.hall@outlook.com>
2018-03-20 11:57:02 +0000

    Added skip button, next button (after selection made, and score label to track the score

--- Project2/GuessTheFlag/GuessTheFlag/ViewController.swift
+++ Project2/GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2018 BeshBashBosh. All rights reserved.
 //
 
+// TODO: - Need to disable the flag buttons after selection is made
+
 import UIKit
 import GameplayKit // Mostly for easy RNG/shuffle
 
@@ -15,10 +17,17 @@ class ViewController: UIViewController {
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
+    @IBOutlet var scoreOutlet: UILabel!
+    @IBOutlet var nextButtonOutlet: UIButton!
     
     // MARK: - Properties
     var countries = [String]() // array to store list of countries in the game
-    var score = 0 // var to keep track of score in game
+    // var to keep track of score in game
+    var score = 0 {
+        willSet {
+            scoreOutlet.text = "Score: \(newValue)"
+        }
+    }
     var correctAnswer = 0 // variable for storing which of 3 flags shown is correct answer
     
     // MARK: - Actions
@@ -35,22 +44,46 @@ class ViewController: UIViewController {
             title = "Uh-oh, incorrect!"
         }
         
-        // 3. Show message telling them new score
+        // Enable and show next button
+        toggleButton(nextButtonOutlet, isActive: true)
+    }
+    
+    // Skip button for skipping turn. Instantiates an alert for are checking.
+    @IBAction func skipButton(_ sender: UIBarButtonItem) {
+        // Alert to check whether the user really wants to do this
         // Use UIAlertController
         // a. instantiate an alert controller
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-        // b. add actions to dismiss
+        let ac = UIAlertController(title: "Skip", message: "Are you sure?", preferredStyle: .alert)
+        // b. add actions to continue or cancel
         // NOTE: if the handler closure is a function it should be without ().
         // If they were included it would run the function rather than pass it.
         // Also the closure is expected to be able to accept a UIAlertAction
         // This has been added as a default argument (=nil)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        ac.addAction(UIAlertAction(title: "Continue", style: .destructive, handler: askQuestion))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         // c. present the AlertController
         present(ac, animated: true)
     }
     
+    // Button for progressing game after guess has been made
+    @IBAction func nextButton(_ sender: UIButton) {
+        toggleButton(sender, isActive: false)
+        askQuestion()
+    }
+    
     // MARK: - Methods
+    // private method for toggling visibility and enabled state of a button
+    private func toggleButton(_ sender: UIButton, isActive: Bool) {
+        sender.isEnabled = isActive
+        sender.isHidden = !isActive
+    }
+    
     func askQuestion(action: UIAlertAction! = nil) {
+        // If this is actioned from a skip button press need to deduct a point
+        if let _ = action {
+            score -= 1
+        }
+        
         // Shuffle the countries array such the the first three elements shown in below button
         // are 1. random, 2. unique. .arrayByShuflfingObjects(in:) returns [Any] so needs typecasting
         countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
@@ -66,12 +99,16 @@ class ViewController: UIViewController {
         button1.setImage(UIImage(named: countries[0]) , for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
     }
     
     // MARK: - View Load Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Hide/disable next button
+        toggleButton(nextButtonOutlet, isActive: false)
         
         // Fill countries array with countries in the game
         countries += ["estonia", "france", "germany", "ireland",
@@ -84,7 +121,6 @@ class ViewController: UIViewController {
             btn.layer.borderWidth = 1
             // Border color
             btn.layer.borderColor = UIColor.lightGray.cgColor
-            
         }
         
         // Gampeplay: ask the question
