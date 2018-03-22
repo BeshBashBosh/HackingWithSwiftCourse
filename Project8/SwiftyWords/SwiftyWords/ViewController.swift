ed341751cbdc3faf4003c38e491c16978c277080
Ben Hall <b.e.s.hall@outlook.com>
2018-03-22 16:39:44 +0000

    Proj7 - Storyboard design and hooking up to VC completed

--- Project8/SwiftyWords/SwiftyWords/ViewController.swift
+++ Project8/SwiftyWords/SwiftyWords/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: -  Outlets
+    @IBOutlet var cluesLabel: UILabel!
+    @IBOutlet var answersLabel: UILabel!
+    @IBOutlet var currentAnswer: UITextField!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    // MARK: - Actions
+    @IBAction func submitTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
