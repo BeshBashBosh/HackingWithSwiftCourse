e076ba098c612a9748b7a90c5dec852ff2f0ff58
Ben Hall <b.e.s.hall@outlook.com>
2018-03-23 12:58:16 +0000

    Proj9: Updated version of Proj7 that uses dispatch queues and threads to run background tasks

--- Project9/GrandCentralDispatch/GrandCentralDispatch/AppDelegate.swift
+++ Project9/GrandCentralDispatch/GrandCentralDispatch/AppDelegate.swift
@@ -12,34 +12,50 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Create second ViewController for tab bar
+        // Storyboard creates a 'window' where we can access the 'rootViewController'.
+        // In our app here that is a tabBarController
+        if let tabBarController = window?.rootViewController as? UITabBarController {
+            // Get access to the storyboard
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            // Create a new view controller within this storyboard wrt the nav controller
+            let vc = storyboard.instantiateViewController(withIdentifier: "NavController")
+            // Create a tab bar item for this new tab
+            vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+            // Add this new controller onto the current list of tab bar vcs
+            tabBarController.viewControllers?.append(vc)
+        }
+        
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+
 
 
 }
--- Project9/GrandCentralDispatch/GrandCentralDispatch/ViewController.swift
+++ Project9/GrandCentralDispatch/GrandCentralDispatch/ViewController.swift
@@ -8,18 +8,140 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var petitions = [[String: String]]() // An array of dictionaries
+    
+    // MARK: - tableView Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("Dequeuing cell at position \(indexPath.row)")
+        let petition = petitions[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"] // The detail label text of the proto cell
+        return cell
+    }
+    
+    // Loading a detail view that hasn't been created in the storyboard
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    // MARK: - Methods
+    // Parse JSON
+    func parse(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            petitions.append(obj)
+        }
+        
+//        // Dispatch this back to the main queue
+//        DispatchQueue.main.async {[unowned self] in
+//            self.tableView.reloadData()
+//        }
+        
+        tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        
+    }
+    
+    // MARK: - Threading Methods
+    @objc func fetchJSON() {
+        // Assign url string of JSON file
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+     
+        // Try to convert above url string into a URL object - that is, check URL is valid
+        if let url = URL(string: urlString) {
+            // Try to get contents of url
+            if let data = try? String(contentsOf: url) {
+                // Use SwiftyJSON to parse the json data into dictionaries
+                let json = JSON(parseJSON: data)
+                
+                // Check the status of the JSON file (response code: 200)
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    print("Good to go!")
+                    self.parse(json: json)
+                    return
+                }
+            }
+        }
+        
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+    
+    // Error messages if processing link to JSON fails
+    @objc func showError() {
+        let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
+//        DispatchQueue.main.async { [unowned self] in
+//            let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+//            ac.addAction(UIAlertAction(title: "OK", style: .default))
+//            self.present(ac, animated: true)
+//        }
 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Refactored code for accessing JSON via background threads
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+        
+//        // Assign url string of JSON file
+//        let urlString: String
+//
+//        if navigationController?.tabBarItem.tag == 0 {
+//            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+//        } else {
+//            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+//        }
+//
+//        // Dispatch the task of getting web content to the userinitiation async queue
+//        DispatchQueue.global(qos: .userInitiated).async { [unowned self] in
+//            // Try to convert above url string into a URL object - that is, check URL is valid
+//            if let url = URL(string: urlString) {
+//                // Try to get contents of url
+//                if let data = try? String(contentsOf: url) {
+//                    // Use SwiftyJSON to parse the json data into dictionaries
+//                    let json = JSON(parseJSON: data)
+//
+//                    // Check the status of the JSON file (response code: 200)
+//                    if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+//                        print("Good to go!")
+//                        self.parse(json: json)
+//                        return
+//                    }
+//                }
+//            }
+//        }
+//
+//        showError()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
