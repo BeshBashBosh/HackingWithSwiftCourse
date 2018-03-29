bb25394c579eada69716a3635529cad4d0141002
Ben Hall <b.e.s.hall@outlook.com>
2018-03-29 12:01:04 +0100

    Proj10 - Added navigation bar iation item to access image picker

--- Project10/NamesToFaces/NamesToFaces/ViewController.swift
+++ Project10/NamesToFaces/NamesToFaces/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+// Note: conformance to ImagePicker and NavController delegate for use with UIImagePickerController
+// Note: When asking for access to photolibrary need to modify the plist appropriately
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     // MARK: - CollectionView Set up
@@ -23,9 +25,54 @@ class ViewController: UICollectionViewController {
         return cell
     }
     
+    // MARK: - Selector methods
+    // Naviagation bar add person action to access photo library
+    @objc func addNewPerson() {
+        // Access the imagepicker
+        let picker = UIImagePickerController()
+        // Allow editing so user can do stuff like cropping
+        picker.allowsEditing = true
+        // Set thus VC as the delegate for the picker
+        picker.delegate = self
+        present(picker, animated: true)
+        
+    }
+    
+    // MARK:- Delegate conformance methods
+    // UIImagePicker "optional" methods (that aren't really optional if you want to do anything with the picker...)
+    // Deal with user having selected an image from photolibrary
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // 1. Extract image from passed in dictionary
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else {return}
+        // 2. Generate unique filename for image (UUID - Universally Unique Identifier)
+        let imageName = UUID().uuidString // generate UUID
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName) // Append to app's documents folderpath
+        // 3. Convert to JPEG, write to disk
+        if let jpegData = UIImageJPEGRepresentation(image, 80) {
+            try? jpegData.write(to: imagePath) // this is a Data object
+        }
+        // 4. Dismiss picker view controller
+        dismiss(animated: true)
+    }
+    
+    // MARK: - Custom methods
+    // Getting the apps document directory
+    func getDocumentsDirectory() -> URL {
+        // Get the app's document directory relative to user's home location
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        // Extract first elements which *should* be user's home directory
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    // MARK: - load methds
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add a navigation button to add photos
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self,
+                                                           action: #selector(addNewPerson))
     }
 
     override func didReceiveMemoryWarning() {
