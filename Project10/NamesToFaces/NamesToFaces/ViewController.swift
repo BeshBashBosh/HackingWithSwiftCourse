060b0b9b73b4de9ad4523c8b9f27291fe3bad0f5
Ben Hall <b.e.s.hall@outlook.com>
2018-03-29 11:20:44 +0100

    Added data source methods for the cds for the collection view

--- Project10/NamesToFaces/NamesToFaces/ViewController.swift
+++ Project10/NamesToFaces/NamesToFaces/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UICollectionViewController {
 
+    
+    // MARK: - CollectionView Set up
+    // Data Source
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Dequeue cell protoype and cast as custom PersonCell type
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
