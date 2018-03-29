6468b37d57cca2b233a0463b7a1f4c3d5b197811
Ben Hall <b.e.s.hall@outlook.com>
2018-03-29 16:20:22 +0100

    Proj10: Finished first implementahed implementation of app. Implemented Person class and adding face+name to collection view. TODO suggetions added.

--- Project10/NamesToFaces/NamesToFaces/ViewController.swift
+++ Project10/NamesToFaces/NamesToFaces/ViewController.swift
@@ -6,6 +6,12 @@
 //  Copyright © 2018 BeshBashBosh. All rights reserved.
 //
 
+// TODO: - 1. AlertController when picture tapped to ask whether to delete or not
+// TODO: - 2. Take an image (picker.sourceType = .camera) instead of selecting from photos (only works on device)
+// can check if such a source type is available with UIImagePickerController.isSourceTypeAvailable
+// TODO: - 3. Need to reload cells already added!
+// Currently they are stored on disk. This is actually fixed in Project 12 UserDefaults
+
 import UIKit
 
 // Note: conformance to ImagePicker and NavController delegate for use with UIImagePickerController
@@ -18,13 +24,51 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     // MARK: - CollectionView Set up
     // Data Source
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // Dequeue cell protoype and cast as custom PersonCell type
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+        
+        // Get person from people array
+        let person = people[indexPath.row]
+        
+        // Set name label of cell to person's name
+        cell.name.text = person.name
+        
+        // create UIImage from person's image filename
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        
+        // Edit cell+contents style
+        cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
         return cell
+    }
+    
+    // cell taps
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // Get person assigned to cell
+        let person = people[indexPath.item]
+        
+        // Create alert controller with text field for name entry, ok, and cancel button
+        let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        ac.addAction(UIAlertAction(title: "OK", style: .default) { [unowned self, ac] _ in
+            let newName = ac.textFields![0]
+            person.name = newName.text!
+            
+            self.collectionView?.reloadData()
+        })
+ 
+        // Then present it
+        present(ac, animated: true)
+        
     }
     
     // MARK: - Selector methods
@@ -33,6 +77,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         // Access the imagepicker
         let picker = UIImagePickerController()
         // Allow editing so user can do stuff like cropping
+        //picker.sourceType = .camera
         picker.allowsEditing = true
         // Set thus VC as the delegate for the picker
         picker.delegate = self
