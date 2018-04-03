c4b43c9112d5dc8e6bd6039a79768c520a60b26a
Ben Hall <b.e.s.hall@outlook.com>
2018-04-03 14:50:06 +0100

    Proj11: Added a background node to the scene

--- Project11/Pachinko/Pachinko/GameScene.swift
+++ Project11/Pachinko/Pachinko/GameScene.swift
@@ -11,11 +11,20 @@ import SpriteKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        // Add a background
+        // Create the background from an image file
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        // Set the centre position of the background node
+        background.position = CGPoint(x: 512, y: 384)
+        // Set the blend mode to one that basically just means "just draw it"
+        background.blendMode = .replace
+        // Set this background to a zPosition truly in the background
+        background.zPosition = -1
+        // Add this node as a child to the scene
+        addChild(background)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        <#code#>
     }
     
 }
