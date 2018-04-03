177d68dee1cb48159206fd33ca527c87d654142c
Ben Hall <b.e.s.hall@outlook.com>
2018-04-03 18:47:31 +0100

    Proj12b: Made the Person class conform to NSCoder

--- Project12a/UserDefaults/UserDefaults/Person.swift
+++ Project12a/UserDefaults/UserDefaults/Person.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class Person: NSObject {
+// NOTE: - NSObject is required when conforming to NSCoding
+class Person: NSObject, NSCoding {
+    
+    // MARK: NSCoder stubs
+    // This is used when saving an object of this class
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+    
+    // Required means that anything that subclasses this must implement this method
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        image = aDecoder.decodeObject(forKey: "image") as! String
+    }
+    
 
     // MARK: Properties
     var name: String
@@ -19,5 +34,6 @@ class Person: NSObject {
         self.name = name
         self.image = image
     }
+    
     
 }
