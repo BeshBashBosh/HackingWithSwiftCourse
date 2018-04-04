d130b205c6ac71246badee35c05af3e8e7a8192f
Ben Hall <b.e.s.hall@outlook.com>
2018-04-04 16:11:15 +0100

    Proj14: Added background and score node

--- Project14/Whack-a-Penguin/Whack-a-Penguin/GameScene.swift
+++ Project14/Whack-a-Penguin/Whack-a-Penguin/GameScene.swift
@@ -11,7 +11,29 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    // MARK: - Properties
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
+        // Set the background
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        // Game score label
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
