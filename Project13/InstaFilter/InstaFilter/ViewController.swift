965a7a5b42e43e90731fa4709ea7d9d1d5b24bc6
Ben Hall <b.e.s.hall@outlook.com>
2018-04-04 13:53:44 +0100

    Proj13: Added CoreImage code to add sepia filter to selected image from imagepicker

--- Project13/InstaFilter/InstaFilter/ViewController.swift
+++ Project13/InstaFilter/InstaFilter/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -16,7 +17,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     // MARK: - Properties
     var currentImage: UIImage!
-    var currentImage2: UIImage?
+    
+    // CoreImage props
+    var context: CIContext!
+    var currentFilter: CIFilter!
     
     // MARK: - Actions
     @IBAction func changeFilter(_ sender: UIButton) {
@@ -28,13 +32,34 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     @IBAction func intensityChanged(_ sender: UISlider) {
+        applyProcessing()
     }
     
     // MARK: - UIImagePicker Methods
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
         dismiss(animated: true)
-        currentImage = image // Copy of the image to revert to
+        currentImage = image // Copy of the image manipulate with CI
+        
+        // CoreImage manipulation
+        // Create a CIImage from a UIImage
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+        
+    }
+    
+    // MARK: - CoreImage Methods
+    func applyProcessing() {
+        // Assign the sliders 'intensity' to control the filters intensity
+        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        // Create a CGImage from the output of the currentFilter, this command actually does the CI processing
+        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+            // Recreate a UIImage of the processed image
+            let processedImage = UIImage(cgImage: cgimg)
+            // Set this processed image to the image view
+            imageView.image = processedImage
+        }
     }
     
     // MARK: - Selector Methods
@@ -56,6 +81,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
                                                             target: self,
                                                             action: #selector(importPicture))
+        
+        // CoreImage
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
         
     }
 
