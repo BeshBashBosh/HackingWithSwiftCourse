342b9f6d8f33e7df312f5885e35db8d56ca7e98a
Ben Hall <b.e.s.hall@outlook.com>
2018-04-04 16:48:50 +0100

    Proj14: Added Penguin nodes to scene and masked them with a maskNode. This will ultimately show give the illusion of them emerging from the slots

--- Project14/Whack-a-Penguin/Whack-a-Penguin/WhackSlot.swift
+++ Project14/Whack-a-Penguin/Whack-a-Penguin/WhackSlot.swift
@@ -11,12 +11,44 @@ import UIKit
 
 class WhackSlot: SKNode {
     
+    // MARK: - Properties
+    var charNode: SKSpriteNode! // A property to store a penguin node
+    
     // Function to add a hole at some input position
     func configure(at position: CGPoint) {
         self.position = position // SKNode's init has a position property that is being modified here
         
         let sprite = SKSpriteNode(imageNamed: "whackHole")
         addChild(sprite)
+        
+        // Create a cropNode slightly higher than hole position
+        // This will be used for masking a penguin, ultimately giving illusion of it appearing from the hole
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        // Position the cropNode in front of other nodes (zPosition -1 for background, 0 default)
+        cropNode.zPosition = 1
+        // Note: Check out the whackMask.png file for what the cropnode looks like.
+        // With such a mask, anything within a coloured region of the mask will be visible.
+        // Anything within a transparent region will be invisible.
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        // The below commented out text shows exactly where the cropmask will be placed in the scene
+//        let test = SKSpriteNode(imageNamed: "whackMask")
+//        test.position = CGPoint(x: 0, y: 15)
+//        test.zPosition = 1
+//        addChild(test)
+        
+        // Create a good penguin node
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        // Start way below the hole
+        charNode.position = CGPoint(x: 0, y: -90)
+        // Give the node a name
+        charNode.name = "character"
+        // Add this node to the cropNode
+        cropNode.addChild(charNode)
+        
+        // Add the cropBode to the scene
+        addChild(cropNode)
     }
 
 }
