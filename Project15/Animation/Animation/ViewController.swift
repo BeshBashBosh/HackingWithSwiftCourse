75035437bd1f2a50f860b1618fbd6030b7c91ad6
Ben Hall <b.e.s.hall@outlook.com>
2018-04-05 13:27:47 +0100

    Proj15: Changed standard animation to springy animation (commented out standard implementation).

--- Project15/Animation/Animation/ViewController.swift
+++ Project15/Animation/Animation/ViewController.swift
@@ -24,8 +24,49 @@ class ViewController: UIViewController {
         // Hide button whilst animation happens so user doesn't attempt multiple animations
         tap.isHidden = true
         
+        // Straight up animation
         // Let animation of UIView have a duration of 1sec starting immediately/
-        UIView.animate(withDuration: 1, delay: 0, options: [],
+//        UIView.animate(withDuration: 1, delay: 0, options: [],
+//                       animations: { [unowned self] in
+//                        // Switch on the animation number to decide which animation to run
+//                        switch self.currentAnimation {
+//                        case 0:
+//                            // Scale 2x in x and y dimension
+//                            self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+//                        case 1:
+//                            // Use identity affine matrix to clear restore to default
+//                            self.imageView.transform = CGAffineTransform.identity
+//                        case 2:
+//                            // Move image -256 points in x and y
+//                            self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+//                        case 3:
+//                            // Restore
+//                            self.imageView.transform = CGAffineTransform.identity
+//                        case 4:
+//                            // Rotate by pi (180deg)
+//                            self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+//                        case 5:
+//                            // Restore
+//                            self.imageView.transform = .identity
+//                        case 6:
+//                            // Reduce transparency/alpha
+//                            self.imageView.alpha = 0.1
+//                            // Change background colour to green
+//                            self.imageView.backgroundColor = .green
+//                        case 7:
+//                            // Restore
+//                            self.imageView.alpha = 1
+//                            self.imageView.backgroundColor = .clear
+//                        default:
+//                            break
+//                        }
+//        }) { [unowned self] (finished: Bool) in
+//            // Animation finished so let's show the tap button again ready for the next animation
+//            self.tap.isHidden = false
+//        }
+        
+        // Springy animation
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [],
                        animations: { [unowned self] in
                         // Switch on the animation number to decide which animation to run
                         switch self.currentAnimation {
