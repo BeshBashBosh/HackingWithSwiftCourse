7e1f007f5d0a9d4ffe078c3b3706ce80ebdea01f
Ben Hall <b.e.s.hall@outlook.com>
2018-04-05 11:46:44 +0100

    Proj15: Added imageview and code to button action to cycle through animation counter

--- Project15/Animation/Animation/ViewController.swift
+++ Project15/Animation/Animation/ViewController.swift
@@ -15,12 +15,26 @@ class ViewController: UIViewController {
     
     // MARK: - Actions
     @IBAction func tapped(_ sender: UIButton) {
+        // Increment the animation
+        currentAnimation += 1
+        // Cycle back to animation 0 eventually
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
     }
     
+    // MARK: - Properties
+    var imageView: UIImageView! // The thing we are going to animate
+    var currentAnimation = 0 // For cycling through animations
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create the imageview in code
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
