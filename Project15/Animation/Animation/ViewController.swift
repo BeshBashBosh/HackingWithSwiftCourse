4237105c0b4ed4a69e8515b8daa4b0363f4760b7
Ben Hall <b.e.s.hall@outlook.com>
2018-04-05 12:02:51 +0100

    Proj15: Added boilerplate a UIView animation code that will ultimately switch on a variable to decide which animation to run

--- Project15/Animation/Animation/ViewController.swift
+++ Project15/Animation/Animation/ViewController.swift
@@ -10,22 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    var imageView: UIImageView! // The thing we are going to animate
+    var currentAnimation = 0 // For cycling through animations
+    
     // MARK: - Outlets
     @IBOutlet var tap: UIButton!
     
     // MARK: - Actions
     @IBAction func tapped(_ sender: UIButton) {
-        // Increment the animation
+        
+        // Animate ui view
+        // Hide button whilst animation happens so user doesn't attempt multiple animations
+        tap.isHidden = true
+        
+        // Let animation of UIView have a duration of 1sec starting immediately/
+        UIView.animate(withDuration: 1, delay: 0, options: [],
+                       animations: { [unowned self] in
+                        // Switch on the animation number to decide which animation to run
+                        switch self.currentAnimation {
+                        case 0:
+                            break
+                            
+                        default:
+                            break
+                        }
+        }) { [unowned self] (finished: Bool) in
+            // Animation finished so let's show the tap button again ready for the next animation
+            self.tap.isHidden = false
+        }
+        
+        // Increment the animation counter
         currentAnimation += 1
+        
         // Cycle back to animation 0 eventually
         if currentAnimation > 7 {
             currentAnimation = 0
         }
+        
     }
-    
-    // MARK: - Properties
-    var imageView: UIImageView! // The thing we are going to animate
-    var currentAnimation = 0 // For cycling through animations
     
     override func viewDidLoad() {
         super.viewDidLoad()
