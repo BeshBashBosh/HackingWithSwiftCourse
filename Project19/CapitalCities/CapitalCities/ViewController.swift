969dd2275869ce6261ee14d6286368671c2db387
Ben Hall <b.e.s.hall@outlook.com>
2018-04-09 17:52:30 +0100

    Proj19: Added MKMapViewDelegate to VC and added stup for adding annotations via viewFor mapView method.

--- Project19/CapitalCities/CapitalCities/ViewController.swift
+++ Project19/CapitalCities/CapitalCities/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     
     // MARK: - Outlets
     @IBOutlet var mapView: MKMapView!
+    
+    // MARK: - MapView Delegate MEthods
+    // Every time map needs to show an annotation, this method is called
+    // Like Table/CollectionViews, MapView reuses annotations views to make best use of memory
+    // BUT, if none available to use, have to create one using MKPinAnnotationView class.
+    // NOTE: viewFor method is called for custom annotations, and Apple's. If Apple's not wanted return nil from viewFor.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // 1. Define reuse identifier for annotation
+        
+        // 2. Check whether annotation being created is custom of one of Apple's.
+        
+        // 3a. Try to dequeue an annotation view from MapView's pool of unused vies
+        
+        // 3b. else create a new one using MKPinAnnotationView and set canShowCallout property to true
+        // (triggers popup of city name)
+        
+        // 4. Create UIButton (.detailDisclosure type, small blue 'i' with circle around it)
+        
+        // 5. If reuse view, update view to use different annotation
+        
+        // 6. If not a Capital annotation, return nil so iOS can use default view
+        return nil
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
