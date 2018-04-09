4a7cf210b47a4c7ad33ea96484acf406eb1b5019
Ben Hall <b.e.s.hall@outlook.com>
2018-04-09 15:25:31 +0100

    Proj18: Added examples of print() debugging to viewDidLoad()

--- Project18/Debugging/Debugging/ViewController.swift
+++ Project18/Debugging/Debugging/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // MARK: - Debug with print
+        print("The simplest way to track a program in to use the 'print()' method")
+        print("This method prints to the console. For example, Hi from the viewDidLoad() method!")
+        print("Often this isn't the best way to debug though...")
+        
+        // But print is quite versatile:
+        print(1, 2, 3, 4, 5) // Printing multiple parameters - print is a variadic funtion that accepts any number of pars
+        print(1, 2, 3, 4, 5, separator: "-") // Can provide a string the separates the printing of the variadic inputs
+        print("Some message", terminator: "") // Can also specify how the line is terminated (defuault line breaks character \n)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
