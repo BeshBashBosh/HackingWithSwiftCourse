27f2ee04186bdecbccfee27fc53e399413d0899a
Ben Hall <b.e.s.hall@outlook.com>
2018-04-10 09:53:28 +0100

    Proj20: Added class properties, function stubs, scene background, and instantiated a timer.

--- Project20/FireworksNight/FireworksNight/GameScene.swift
+++ Project20/FireworksNight/FireworksNight/GameScene.swift
@@ -10,8 +10,48 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    // MARK: - Properties
+    var gameTime: Timer! // A timer!
+    var fireworks = [SKNode]() // An array to track the fireworks currently active in the scene
+    
+    // Limits of where fireworks can be launched from
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    
+    // Track players score
+    var score = 0 {
+        didSet {
+            // This will undoubtedly update a score node label
+        }
+    }
+    
+    // MARK: - Custom Methods
+    // Creates firework node in scene
+    func createFirework() {
         
+    }
+    
+    // Method to call createFirework() and launch one into scene
+    @objc func launchFirework() {
+        
+    }
+    
+    // MARK: - SpriteKit methods
     override func didMove(to view: SKView) {
+        // Add background node to the scene
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        // Create timer to launch fireworks every 6 seconds\
+        gameTime = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFirework),
+                                        userInfo: nil, repeats: true)
+        
+        
     }
     
     
