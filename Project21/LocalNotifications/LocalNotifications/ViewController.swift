367ebb504dc61a976298593e2c9349d45e0a4c34
Ben Hall <b.e.s.hall@outlook.com>
2018-04-10 16:56:04 +0100

    Proj21: implemented registerLocal() selector method to request user authorization of sending local notifications.

--- Project21/LocalNotifications/LocalNotifications/ViewController.swift
+++ Project21/LocalNotifications/LocalNotifications/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     // MARK: - Selector Methods
+    // This method will request permission to post local notifications
     @objc func registerLocal() {
+        // Get an instance of the notificaiton center
+        let center = UNUserNotificationCenter.current()
+        
+        // Request autho
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay")
+            } else {
+                print("Neh")
+            }
+        }
         
     }
     
@@ -24,6 +37,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         // Add navigation bar items
+        // Button to register intent to post local notifications
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self,
                                                            action: #selector(registerLocal))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self,
