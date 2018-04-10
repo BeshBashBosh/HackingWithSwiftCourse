5d17e290df7514c925f80295dad5a4fe57b112dc
Ben Hall <b.e.s.hall@outlook.com>
2018-04-10 17:22:03 +0100

    Proj21: Implemented scheduleLocal() selector method to schedule a notification 5 sec later.

--- Project21/LocalNotifications/LocalNotifications/ViewController.swift
+++ Project21/LocalNotifications/LocalNotifications/ViewController.swift
@@ -28,8 +28,40 @@ class ViewController: UIViewController {
         
     }
     
+    // Configures all data needed to schedule a notification
     @objc func scheduleLocal() {
+        // Get instance of NC
+        let center = UNUserNotificationCenter.current()
         
+        // This removes any pending notification requests
+        center.removeAllPendingNotificationRequests()
+        
+        // Configure content (what to show)
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese"
+        content.categoryIdentifier = "alarm" // custom actions
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default()
+        
+        // Configure trigger (when to show)
+        // Calendar notif
+        // The below is a repeating notification at 10:30am everyday
+//        var dateComponents = DateComponents()
+//        dateComponents.hour = 10
+//        dateComponents.minute = 30
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        // This trigger happens after 5 seconds
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        // Configure request (combination of content + trigger)
+        // Notifications can have a unique identifier that allows you to programmatically update or remove notifs.
+        // For example, if the notification was a score update, it would be better to update the notif rather than send
+        // new ones when the score changes.
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        // Add request to the center
+        center.add(request)
     }
     
     override func viewDidLoad() {
