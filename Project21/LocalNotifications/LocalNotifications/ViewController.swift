5cd298a3d645c3344d7f4b185527d3a23206ff70
Ben Hall <b.e.s.hall@outlook.com>
2018-04-10 15:15:03 +0100

    Proj21: Added nagitation controller along with two bar button items. These buttons have selector method stubs set aside and will initiate local notifications.

--- Project21/LocalNotifications/LocalNotifications/ViewController.swift
+++ Project21/LocalNotifications/LocalNotifications/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Selector Methods
+    @objc func registerLocal() {
+        
+    }
+    
+    @objc func scheduleLocal() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add navigation bar items
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self,
+                                                           action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self,
+                                                            action: #selector(scheduleLocal)
+        )
     }
 
     override func didReceiveMemoryWarning() {
