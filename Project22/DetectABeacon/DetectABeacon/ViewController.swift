9d96a3065ee6c96143d43da418014cff364fc00c
Ben Hall <b.e.s.hall@outlook.com>
2018-04-11 10:14:34 +0100

    Proj22: Added location privacy ket to Info.plist

--- Project22/DetectABeacon/DetectABeacon/ViewController.swift
+++ Project22/DetectABeacon/DetectABeacon/ViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2018 BeshBashBosh. All rights reserved.
 //
 
+// NOTE: To request access to a users location. Need to visit the info.plist
+//       Need to add item "Privacy - Location Always Usage Description" (location even when app not running)
+//                        "Privacy - Location When In Use Usage Description" (loc only when app running)
+//                        Type should be set to string with a value explaining why loc needed.
+
 import UIKit
 
 class ViewController: UIViewController {
