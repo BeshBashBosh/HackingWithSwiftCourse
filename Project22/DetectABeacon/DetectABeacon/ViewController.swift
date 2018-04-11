bd8d18709188218ddf013bab8d00c97357ddba66
Ben Hall <b.e.s.hall@outlook.com>
2018-04-11 11:12:17 +0100

    Proj22: Addedcode to request location discovery and pass that to didChangeAuthorization CL delegate method to check and find if beacon ranging available

--- Project22/DetectABeacon/DetectABeacon/ViewController.swift
+++ Project22/DetectABeacon/DetectABeacon/ViewController.swift
@@ -9,15 +9,48 @@
 // NOTE: To request access to a users location. Need to visit the info.plist
 //       Need to add item "Privacy - Location Always Usage Description" (location even when app not running)
 //                        "Privacy - Location When In Use Usage Description" (loc only when app running)
+//                        "Privacy - Location Always and When In Use Usage Description" (noth of above)
 //                        Type should be set to string with a value explaining why loc needed.
+//        NOTE: If third option used, need to also include the second option with a value
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // MARK: - Outlets
+    @IBOutlet var distanceReading: UILabel! // This will change based on distance to iBeacon
+    
+    // MARK: - Properties
+    var locationManager: CLLocationManager! // will hold an instance to the CoreLocation manager
+
+    // MARK: - CoreLocation Delegate Methods
+    // This is called when user makes a decision on authorization request
+    // Can handle the change in authorization here
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways { // Check it's always authorized
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) { // can the device monitor iBeacons?
+                if CLLocationManager.isRangingAvailable() { // Is ranging of iBeacons possible?
+                    // do stuff
+                    print("Yarp")
+                }
+            }
+        } else {
+            print("Not always authorized")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Request user authorization for location
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization() // or .requestWhenInUseAuthorization() if that add to .plist
+        
+        // Set view background color to gray
+        view.backgroundColor = .gray
     }
 
     override func didReceiveMemoryWarning() {
