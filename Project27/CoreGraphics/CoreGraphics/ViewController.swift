5aa8c85a9615bfaa274b8bc37e67631a409a54a4
Ben Hall <b.e.s.hall@outlook.com>
2018-04-12 15:00:47 +0100

    Proj27: Implemented code for button taps that cycles through CoreGraphic drawing methods.

--- Project27/CoreGraphics/CoreGraphics/ViewController.swift
+++ Project27/CoreGraphics/CoreGraphics/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    var currentDrawType = 0 // This will be used to cycle through CoreGraphics
+    
+    // MARK: - Custom methods
+    func drawRectangle() {
+        
+    }
+    
     // MARK: - Outlets
     @IBOutlet var imageView: UIImageView!
     
     // MARK: - Actions
-    @IBOutlet var redrawTapped: UIButton!
-    
-    
+    @IBAction func redrawTapped(_ sender: UIButton) {
+        // Increment the drawing type
+        currentDrawType += 1
+        
+        // Recycle draw type after 5 iterations
+        if currentDrawType > 5 { currentDrawType = 0 }
+        
+        // Switch on draw type to see how to draw
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        drawRectangle()
     }
 
     override func didReceiveMemoryWarning() {
