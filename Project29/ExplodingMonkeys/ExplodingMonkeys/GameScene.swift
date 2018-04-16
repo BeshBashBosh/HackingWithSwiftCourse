c413c8b3ca719126887b69f8023ffe6836a4a6d6
Ben Hall <b.e.s.hall@outlook.com>
2018-04-16 17:03:15 +0100

    Proj29: In GameScene.swift: Added a background, added method that uses BuildingNode custom class to add randomised buildings within th scene.

--- Project29/ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
+++ Project29/ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
@@ -18,10 +18,44 @@ enum CollisionTypes: UInt32 {
 
 class GameScene: SKScene {
     
-
+    // MARK: - Properties
+    var buildings = [BuildingNode]()
     
-    override func didMove(to view: SKView) {
+    // MARK: - Set-up methods
+    func createBuildings() {
+        // add buildings to scene spaced horizontally starting slighlty off screen (x: -15) up until far edge
+        // give 2 point gap between buildings
+        
+        // Buildings will be randome size.
+        // Height -> 300 - 600
+        // Width -> Divide evenly by 40 (for window drawing) (gen no. between 2 ... 4 and x40)
+        
+        var currentX: CGFloat = -15
+        while currentX < 1024 {
+            let size = CGSize(width: RandomInt(min: 2, max: 4) * 40,
+                              height: RandomInt(min: 300, max: 600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.red, size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+        }
+        
 
+        
+        
+    }
+    
+    // MARK: - SK lifecycle methods
+    override func didMove(to view: SKView) {
+        // Create a background
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        
+        // Create buildings
+        createBuildings()
     }
     
 
