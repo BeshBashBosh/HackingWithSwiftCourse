9ae5e1b64e43528224eccdfdf9db2cc3a606c7a0
Ben Hall <b.e.s.hall@outlook.com>
2018-04-19 17:18:58 +0100

    Proj32: Added a function that makes an NSAttributedString out of the title and subtitle parts of the tableview cell text. This itself uses the devices dynamic type for font sizing

--- Project32/SwiftSearcher/SwiftSearcher/ViewController.swift
+++ Project32/SwiftSearcher/SwiftSearcher/ViewController.swift
@@ -8,16 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    // MARK: - Properties
+    var projects = [[String]]()
+    
+    // MARK: - CustomMethods
+    func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
+        // Create NSAtrribute arrays for title and subtitle string
+        let titleAttributes = [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: .headline),
+                               NSAttributedStringKey.foregroundColor: UIColor.purple]
+        let subtitleAttributes = [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: .subheadline)]
+        
+        // Create title and subtitle NSStrings using these attributes
+        let titleString = NSMutableAttributedString(string: "\(title)\n", attributes: titleAttributes)
+        let subtitleString = NSMutableAttributedString(string: subtitle, attributes: subtitleAttributes)
+        
+        // Join these attribute strings together and return
+        titleString.append(subtitleString)
+        return titleString
+    }
+    
+    // MARK: - TableView methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return projects.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) // dequeue cell
+        let project = projects[indexPath.row] // grab project at cell index
+        //cell.textLabel?.text = "\(project[0]): \(project[1])" // set cells text to project
+        cell.textLabel?.attributedText = makeAttributedString(title: project[0], subtitle: project[1])
+        return cell // return cell
+    }
+    
+    // MARK: - VC Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add projects to projects array
+        projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
+        projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
+        projects.append(["Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, the Social framework, URL"])
+        projects.append(["Project 4: Easy Browser", "loadView(), WKWebView, delegation, classes and structs, URLRequest, UIToolbar, UIProgressView., key-value observing"])
+        projects.append(["Project 5: Word Scramble", "Closures, method return values, booleans, NSRange"])
+        projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
+        projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
+        projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+  
     }
 
 
