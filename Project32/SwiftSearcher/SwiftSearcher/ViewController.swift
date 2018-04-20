b82e69104badf8a9cbd2ee5795dc9d51fb006786
Ben Hall <b.e.s.hall@outlook.com>
2018-04-20 10:23:10 +0100

    Proj32: Updated tableview to editing mode to allow for adding favorites via + and - buttons. This is stored and restored from UserDefaults.

--- Project32/SwiftSearcher/SwiftSearcher/ViewController.swift
+++ Project32/SwiftSearcher/SwiftSearcher/ViewController.swift
@@ -12,7 +12,8 @@ import SafariServices
 class ViewController: UITableViewController {
 
     // MARK: - Properties
-    var projects = [[String]]()
+    var projects = [[String]]() // stores the details on the HwS projects presented int eh TableView
+    var favorites = [Int]() // Stores which HwS projects have been favorited by user
     
     // MARK: - CustomMethods
     func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
@@ -41,6 +42,14 @@ class ViewController: UITableViewController {
         }
     }
     
+    func index(item: Int) {
+        
+    }
+    
+    func deindex(item: Int) {
+        
+    }
+    
     // MARK: - TableView methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return projects.count
@@ -51,12 +60,55 @@ class ViewController: UITableViewController {
         let project = projects[indexPath.row] // grab project at cell index
         //cell.textLabel?.text = "\(project[0]): \(project[1])" // set cells text to project
         cell.textLabel?.attributedText = makeAttributedString(title: project[0], subtitle: project[1])
+        
+        // Add a favorites check mark if it exists in saved storage
+        if favorites.contains(indexPath.row) {
+            cell.editingAccessoryType = .checkmark
+        } else {
+            cell.editingAccessoryType = .none
+        }
+        
         return cell // return cell
     }
     
     // cell tapped, do something!
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         showTutorial(indexPath.row)
+    }
+    
+    // Inform the tableview which rows should have an "insert" or "delete" icon
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        if favorites.contains(indexPath.row) {
+            return .delete // remove favorite
+        } else {
+            return .insert // add favorite
+        }
+    }
+    
+    // Method tells table what to do when user clicks insert or delete icon on each row
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        // Switch on the editingStyle to determine whether adding or removing favorite
+        switch editingStyle {
+        case .delete:
+            if let index = favorites.index(of: indexPath.row) {
+                favorites.remove(at: index)
+                deindex(item: indexPath.row)
+            }
+        case .insert:
+            favorites.append(indexPath.row)
+            index(item: indexPath.row)
+        default:
+            return
+        }
+        
+        // Save the new favorites to user defaults
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+        
+        // Reload the tableview at editing row (no need to reload entire thing!)
+        tableView.reloadRows(at: [indexPath], with: .none)
+        
     }
     
     // MARK: - VC Lifecycle methods
@@ -73,6 +125,18 @@ class ViewController: UITableViewController {
         projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
         projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
         projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
+        
+        // Load favorited projects (if any yet) from UserDefaults
+        let defaults = UserDefaults.standard
+        if let savedFavorites = defaults.object(forKey: "favorites") as? [Int] {
+            favorites = savedFavorites
+        }
+        
+        // Put tableview in editing mode
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true // Allows us to still select an entry when in edit mode
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
