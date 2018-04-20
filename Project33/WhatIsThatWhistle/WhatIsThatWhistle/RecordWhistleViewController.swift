a9c6007b6448896ad83c022e170dd3f4e9c311ee
Ben Hall <b.e.s.hall@outlook.com>
2018-04-20 12:01:37 +0100

    Proj33: Added navbar items to main VC, one of which pushes a custom RecordWhistleViewController to the user. This has been created with a stackview to manage subviews.

---
+++ Project33/WhatIsThatWhistle/WhatIsThatWhistle/RecordWhistleViewController.swift
@@ -0,0 +1,60 @@
+//
+//  RecordWhistleViewController.swift
+//  WhatIsThatWhistle
+//
+//  Created by Ben Hall on 20/04/2018.
+//  Copyright © 2018 BeshBashBosh. All rights reserved.
+//
+
+import UIKit
+
+class RecordWhistleViewController: UIViewController {
+
+    // MARK: - Properties
+    var stackView: UIStackView!
+    
+    // MARK: - VC Lifecycle methods
+    // Do all view loading in code
+    override func loadView() {
+        super.loadView()
+        
+        view.backgroundColor = .gray // set color of main view
+        
+        // Create the stackview
+        stackView = UIStackView()
+        stackView.spacing = 30
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        // Constraints for stackview
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project33/WhatIsThatWhistle/WhatIsThatWhistle/ViewController.swift
+++ Project33/WhatIsThatWhistle/WhatIsThatWhistle/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Selector methods
+    // Pushes a RecordWhistleViewController to user to record a whistle
+    @objc func addWhistle() {
+        let vc = RecordWhistleViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    // MARK: - VC Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set up navbar
+        title = "What is that Whistle?"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self,
+                                                            action: #selector(addWhistle))
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil,
+                                                           action: nil)
     }
 
     override func didReceiveMemoryWarning() {
