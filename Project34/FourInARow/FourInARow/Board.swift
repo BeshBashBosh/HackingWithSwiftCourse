765a740d1f99a479f6bdebddba104631a56a7a21
Ben Hall <b.e.s.hall@outlook.com>
2018-04-25 17:17:57 +0100

    Proj34: Added Board.swift model file that handles the game board, chips in play, and where chips can be palced

---
+++ Project34/FourInARow/FourInARow/Board.swift
@@ -0,0 +1,76 @@
+//
+//  Board.swift
+//  FourInARow
+//
+//  Created by Ben Hall on 25/04/2018.
+//  Copyright © 2018 BeshBashBosh. All rights reserved.
+//
+//  Model file - Storeswhere the chips are and who is winning
+
+import UIKit
+
+// MARK: - Enums
+
+// This enum defines the types of chips that can be in play
+enum ChipColor: Int {
+    case none = 0
+    case red
+    case black
+}
+
+class Board: NSObject {
+
+    // MARK: - Class Properties
+    static var width = 7
+    static var height = 6
+    
+    // MARK: - Instance Properties
+    var slots = [ChipColor]()
+    
+    // MARK: - Inits
+    override init() {
+        // Set all slots available on the board to have no chips in them
+        for _ in 0 ..< Board.width * Board.height {
+            slots.append(.none)
+        }
+        
+        super.init()
+    }
+    
+
+    
+    
+    // MARK: - Instance methods
+    
+    // Gets what chip type exists at a particular column and row
+    func chip(inColumn column: Int, row: Int) -> ChipColor {
+        return slots[row + column * Board.height]
+    }
+    
+    // Sets what chip type is in a specific column and row
+    func set(chip: ChipColor, in column: Int, row: Int) {
+        slots[row + column * Board.height] = chip
+    }
+    
+    // Determine if a player can place a chip in a particular slot
+    // For an input column, returns which row a chip can be placed (if at all, hence optional)
+    func nextEmptySlot(in column: Int) -> Int? {
+        for row in 0 ..< Board.height {
+            if chip(inColumn: column, row: row) == .none {
+                return row
+            }
+        }
+        
+        return nil
+    }
+    
+    func canMove(in column: Int) -> Bool {
+        return nextEmptySlot(in: column) != nil
+    }
+    
+    func add(chip: ChipColor, in column: Int) {
+        if let row = nextEmptySlot(in: column) {
+            set(chip: chip, in: column, row: row)
+        }
+    }
+}
--- Project34/FourInARow/FourInARow/ViewController.swift
+++ Project34/FourInARow/FourInARow/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    var placedChips = [[UIView]]()
+    var board: Board!
+    
     // MARK: - Outlets
     @IBOutlet var columnButtons: [UIButton]!
     
@@ -21,7 +25,14 @@ class ViewController: UIViewController {
     // MARK: - VC Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add empty UIView arrays to placedChips array for each board column
+        for _ in 0 ..< Board.width {
+            placedChips.append([UIView]())
+        }
+        
+        // Reset the board
+        resetBoard()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +40,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: - Instance Methods
+    
+    // Method to reset the board at start of new game
+    func resetBoard() {
+        // Create new instance of the board
+        board = Board()
+        
+        // Remove any already existing chips from play
+        for i in 0 ..< placedChips.count {
+            for chip in placedChips[i] {
+                chip.removeFromSuperview()
+            }
+            
+            // Remove the chips from the placedChips array
+            placedChips[i].removeAll(keepingCapacity: true)
+        }
+    }
 
 }
 
