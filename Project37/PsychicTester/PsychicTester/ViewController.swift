8eb66b5fab74f07c538e907af261a81b5a1acc63
Ben Hall <b.e.s.hall@outlook.com>
2018-04-30 17:55:55 +0100

    Proj37: Added UIView container storyboard that will hold custom views. Created outlet to this in ViewController class.

--- Project37/PsychicTester/PsychicTester/ViewController.swift
+++ Project37/PsychicTester/PsychicTester/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet var cardContainer: UIView!
+    
+    // MARK: - VC Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
