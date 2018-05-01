1108b55048fa06b1ab7c659926f383e3a8af770e
Benjamin Hall <b.e.s.hall@outlook.com>
2018-05-01 23:45:48 +0100

    Proj37: Created a WatchOS UI that consists of label and button to set up app. Linked communication with iOS app. Need to test on device for haptic feedback between two devices. Added future TODO suggestions

--- Project37/PsychicTester/PsychicTester WatchKit Extension/InterfaceController.swift
+++ Project37/PsychicTester/PsychicTester WatchKit Extension/InterfaceController.swift
@@ -8,24 +8,56 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
 
-class InterfaceController: WKInterfaceController {
-
+    // MARK: - Outlets
+    @IBOutlet var welcomeText: WKInterfaceLabel!
+    @IBOutlet var hideButton: WKInterfaceButton!
+    
+    // MARK: - Actions
+    // Hides text and button
+    @IBAction func hideWelcomeText() {
+        welcomeText.setHidden(true)
+        hideButton.setHidden(true)
+    }
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
     }
     
+    // This is equivalent to viewDidLoad()...
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        print("WatchOS - willActivate()")
+        // Create Watch Connectivity session matching the iOS app for communication between the two.
+        if (WCSession.isSupported()) {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+            print("WatchOS - Session Activated")
+        }
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    // MARK: - Communication with iOS app methods
+    // Will be called when message is received from iOS app
+    func session(_ session: WCSession, didReceiveMessageData messageData: Data) {
+        WKInterfaceDevice().play(.click)
+    }
+    
+    // MARK: - WCSessionDelegate conformity stubs
+    // needed for protocol conformity, but not going to use so left empty...
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
     }
 
 }
--- Project37/PsychicTester/PsychicTester/ViewController.swift
+++ Project37/PsychicTester/PsychicTester/ViewController.swift
@@ -5,6 +5,9 @@
 //  Created by Ben Hall on 30/04/2018.
 //  Copyright © 2018 BeshBashBosh. All rights reserved.
 //
+// TODO: - Implement sessionWatchStateDidChange() to detect when watch goes to sleep
+//         Get the app to play bried innocuous sound alerting you to wake the watch
+// TODO: - Add hidden button to watch app that is always win button. It changes all cards to stars
 
 import UIKit
 import GameplayKit
@@ -214,10 +217,23 @@ class ViewController: UIViewController, WCSessionDelegate {
             session.delegate = self
             // activate it
             session.activate()
+            print("iOS - Session activated")
         }
 
     }
 
+    // Give instructions to user about what to do
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let instructions = "Please ensure your Apple Watch is configured correctly. On your iPhone, launch Apple's 'Watch' configuration app then choose General > Wake Screen. On that screen, please disable Wake Screen On Wrist Raise, then select Wake For 70 Seconds. On your Apple Watch, please swipe up on your watch face and enable Silent Mode. You're done!"
+        let ac = UIAlertController(title: "Adjust your settings", message: instructions,
+                                   preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
