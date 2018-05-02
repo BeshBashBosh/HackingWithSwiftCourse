858f3913c3ac33acc0983aab0c6a509d36be5b60
Benjamin Hall <b.e.s.hall@outlook.com>
2018-05-02 21:04:22 +0100

    Proj38: Refactored ViewController methods to use NSFetchedResultsController for fetching results, updating the model, and the UI.

--- Project38/GitHubCommit/GitHubCommit/ViewController.swift
+++ Project38/GitHubCommit/GitHubCommit/ViewController.swift
@@ -13,9 +13,15 @@ class ViewController: UITableViewController, NSFetchedResultsControllerDelegate 
 
     // MARK: - Properties
     var container: NSPersistentContainer! // manages connection to SQLite database
-    var commits = [Commit]() // will store Commit objects extracted from CoreData DB.
+    
+    // ===============================================================================
+    // REMOVED AS NSFectchedResultsControllerDelegate manages this now
+    //var commits = [Commit]() // will store Commit objects extracted from CoreData DB
+    // ===============================================================================
+    
     var commitPredicate: NSPredicate? // Will allow us to filter our DB requests
     
+    // property to optimize the entire fetch, storage, and UI sync processes
     var fetchedResultsController: NSFetchedResultsController<Commit>!
     
     // MARK: - Custom methods
@@ -132,28 +138,41 @@ class ViewController: UITableViewController, NSFetchedResultsControllerDelegate 
     
     // Creates request to database and fetches results
     private func loadSavedData() {
+        
+        // If no NSFetchedResultsController exists yet...
         if fetchedResultsController == nil {
+            // Create Commit managed object fetch request
             let request = Commit.createFetchRequest()
+            // Apply sorting description
             let sort = NSSortDescriptor(key: "date", ascending: false)
             request.sortDescriptors = [sort]
+            // Only fetch in batches of 20 at a time
             request.fetchBatchSize = 20
             
+            // Instantiate the NSFetchedResultsController with this fetch request type
+            // and the GHCommit container context
             fetchedResultsController = NSFetchedResultsController(fetchRequest: request,
                                                                   managedObjectContext: container.viewContext,
                                                                   sectionNameKeyPath: nil, cacheName: nil)
+            // Set NSFetchedResultsControllerDelegate to this VC
             fetchedResultsController.delegate = self
         }
         
+        // Apple the predicate filter
         fetchedResultsController.fetchRequest.predicate = commitPredicate
         
+        // Attempt fetch request
         do {
             try fetchedResultsController.performFetch()
             tableView.reloadData()
         } catch {
-            print("Fetch failed")
+            //Error in fetching, present alert to user
+            self.createAlertWith(title: "DB Fetch Error", message: "There was an error fetching from the database. \(error.localizedDescription)")
         }
         
+        //==========================================================
         // PRE NSFetchedResultsController Implementation
+        //==========================================================
 //        // Create a fetch request from the database object type
 //        let request = Commit.createFetchRequest()
 //        // Describe how to sort fetched results (by date descending)
@@ -173,7 +192,6 @@ class ViewController: UITableViewController, NSFetchedResultsControllerDelegate 
 //            // Error in fetching, present alert to user
 //            self.createAlertWith(title: "DB Fetch Error", message: "There was an error fetching from the database. \(error.localizedDescription)")
 //        }
-        
         
     }
 
@@ -268,17 +286,21 @@ class ViewController: UITableViewController, NSFetchedResultsControllerDelegate 
 
     // MARK: Default TableVC methods
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        //return 1
+        return fetchedResultsController.sections?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return commits.count
+        //return commits.count
+        let sectionInfo = fetchedResultsController.sections![section]
+        return sectionInfo.numberOfObjects
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Commit", for: indexPath)
         
-        let commit = commits[indexPath.row]
+        //let commit = commits[indexPath.row]
+        let commit = fetchedResultsController.object(at: indexPath)
         cell.textLabel!.text = commit.message
         cell.detailTextLabel!.text = "By \(commit.author.name) on \(commit.date.description)"
         
@@ -288,7 +310,7 @@ class ViewController: UITableViewController, NSFetchedResultsControllerDelegate 
     // Pass control to DetailViewController
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            vc.detailItem = self.commits[indexPath.row]
+            vc.detailItem = fetchedResultsController.object(at: indexPath)
             navigationController?.pushViewController(vc, animated: true)
         }
     }
@@ -296,11 +318,32 @@ class ViewController: UITableViewController, NSFetchedResultsControllerDelegate 
     // Commit insertion or deletion of row by user
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let commit = commits[indexPath.row] // extract the Commit object
+            //let commit = commits[indexPath.row] // extract the Commit object
+            let commit = fetchedResultsController.object(at: indexPath)
             container.viewContext.delete(commit) // delete from the GHCommit container context
-            commits.remove(at: indexPath.row) // remove from VC instance parameter array
-            tableView.deleteRows(at: [indexPath], with: .fade) // delete from tableView
+            
+            // The NSFetchedResultsViewController is clever and has knowledge of the state of the viewContext.
+            // so when something is removed from it, it acts to update the UI!
+            // NOTE - WE DO HAVE TO IMPLEMENT SUCH A METHOD... (see below)
+            
+            //commits.remove(at: indexPath.row) // remove from VC instance parameter array
+            //tableView.deleteRows(at: [indexPath], with: .fade) // delete from tableView
             self.saveContext() // save to context
+        }
+    }
+    
+    // MARK: - NSFetchedResultsViewController methods
+    // When object changes, this gets called by the NSFetchedResultsVC
+    // This method is called from anywhere! For example, if we deleted the object from the DetailViewController
+    // it would still update. THE DATA DRIVES THE VIEW RATHER THAN VIEW DRIVES THE DATA
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any, at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .automatic)
+        default:
+            break
         }
     }
 
