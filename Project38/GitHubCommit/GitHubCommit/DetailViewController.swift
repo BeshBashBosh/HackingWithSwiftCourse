22ee10e5e01daea878461404397e5ffb803e501b
Ben Hall <b.e.s.hall@outlook.com>
2018-05-02 09:48:43 +0100

    Proj38 - Set up basic template for project. Incl. creating UITableViewController as main VC (embedded in navbarcontroller) and added DetailViewController that will be called when tapping table cells.

---
+++ Project38/GitHubCommit/GitHubCommit/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  GitHubCommit
+//
+//  Created by Ben Hall on 02/05/2018.
+//  Copyright © 2018 BeshBashBosh. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet var detailLabel: UILabel!
+    
+    // MARK: - Default VC Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
