5d496f03c74e5e5a2fb19381cdad7ca0d87a9404
Ben Hall <b.e.s.hall@outlook.com>
2018-05-02 16:17:01 +0100

    Proj38: Updated GHCommit core data model to include Author Entity with email and name attributes. The name attr has been set as an indexed value. Relationship created between Author and Commit (one(Author)-to-many(Commit)). Created NSManaged classes for these Entities.

---
+++ Project38/GitHubCommit/GitHubCommit/Author+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Author+CoreDataClass.swift
+//  GitHubCommit
+//
+//  Created by Ben Hall on 02/05/2018.
+//  Copyright © 2018 BeshBashBosh. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Author)
+public class Author: NSManagedObject {
+
+}
---
+++ Project38/GitHubCommit/GitHubCommit/Author+CoreDataProperties.swift
@@ -0,0 +1,41 @@
+//
+//  Author+CoreDataProperties.swift
+//  GitHubCommit
+//
+//  Created by Ben Hall on 02/05/2018.
+//  Copyright © 2018 BeshBashBosh. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Author {
+
+    @nonobjc public class func createFetchRequest() -> NSFetchRequest<Author> {
+        return NSFetchRequest<Author>(entityName: "Author")
+    }
+
+    @NSManaged public var email: String
+    @NSManaged public var name: String
+    @NSManaged public var commits: NSSet
+
+}
+
+// MARK: Generated accessors for commits
+extension Author {
+
+    @objc(addCommitsObject:)
+    @NSManaged public func addToCommits(_ value: Commit)
+
+    @objc(removeCommitsObject:)
+    @NSManaged public func removeFromCommits(_ value: Commit)
+
+    @objc(addCommits:)
+    @NSManaged public func addToCommits(_ values: NSSet)
+
+    @objc(removeCommits:)
+    @NSManaged public func removeFromCommits(_ values: NSSet)
+
+}
