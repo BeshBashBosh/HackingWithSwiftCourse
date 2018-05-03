75033d2f108a9da979ca2838b05981bd65e20a55
Ben Hall <b.e.s.hall@outlook.com>
2018-05-03 10:39:28 +0100

    Proj39: Added wordsCount: [String: Int] property to PlayData that calculates and stores the occurrence of each word in all of Shakespeare's works. The keys of this have been used to remove duplicate word entries from PlayData allWords property.

--- Project39/UnitTest/UnitTest/PlayData.swift
+++ Project39/UnitTest/UnitTest/PlayData.swift
@@ -11,14 +11,29 @@ import Foundation
 
 struct PlayData {
     var allWords = [String]()
+    var wordsCount = [String: Int]()
     
     init() {
         if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
             if let plays = try? String(contentsOfFile: path) {
+                
                 // Use CharacterSet to separate read-in string by anything that isn't a letter or number (.inverted)
                 allWords = plays.components(separatedBy: CharacterSet.alphanumerics.inverted)
+                
                 // Filter out blank characters
                 allWords = allWords.filter { $0 != "" }
+                
+                // Count up occurrence of unique words
+                for word in allWords {
+                    if wordsCount[word] == nil {
+                        wordsCount[word] = 1
+                    } else {
+                        wordsCount[word]! += 1
+                    }
+                }
+                
+                // Remove duplicate words from allWords array
+                allWords = Array(wordsCount.keys)
             }
         }
     }
